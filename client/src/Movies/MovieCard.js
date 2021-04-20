f086d4111ebde9ef3bc9fff4ff4c4026244ec3bd
Austin Carman <abcarman12@gmail.com>
2021-04-19 18:37:33 -0600

    finished all MVP

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,37 @@
 import React from 'react';
 
 export default function MovieCard (props) {
-  return;
+  return(
+    <div className="save-wrapper">
+      <Link to={`/movies/${id}`}>
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+        </div>
+      </Link>
+      <Router path={`/movies/${id}`}>
+        <ActorList />
+      </Router>
+    </div>
+  );
+}
+
+function ActorList(props) {
+  const { title, director, metascore, id } = props.movie;
+  return (
+    <>
+      <h3>Actors</h3>
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+      <div className="save-button">Save</div>
+    </>
+  );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails key={movie.id} movie={movie} /> //pass in movieCard here?
+        // <MoiveCard key={movie.id} movie=
       ))}
     </div>
   );
 }
 
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+function MovieDetails(props) { // Comment out this function and use MovieCard instead?
+  const { title, director, metascore, id } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+    <Link to={`/movies/${id}`}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    </Link>
   );
 }
+
+
